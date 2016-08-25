367988bcec7deca67225419db8302223f8e9d258
HenryZhou7 <zhouyh.7@gmail.com>
2016-08-25 11:20:42 +0800

    Initial Commit

---
+++ auto_timetable/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  auto_timetable
+//
+//  Created by Henry Zhou on 8/25/16.
+//  Copyright © 2016 Henry Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
